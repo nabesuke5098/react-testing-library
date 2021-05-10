61d5555897f720037d79da56543ad09fe5e08188
nabesuke <nabesuke5098@gmail.com>
2021-05-10 09:05:21 +0900

    [add]ExtraReducer.test.js

---
+++ src/ExtraReducer.test.js
@@ -0,0 +1,16 @@
+import { isAsyncThunkAction } from "@reduxjs/toolkit";
+import reducer, {
+  fetchDummy,
+} from "../src/features/customCounter/customCounterSlice";
+
+describe("extraReducers", () => {
+  const initialState = {
+    mode: 0,
+    value: 0,
+  };
+  it("Should output 100 + payload when fulfilled", () => {
+    const action = { type: fetchDummy.fulfilled.type, payload: 5 };
+    const state = reducer(initialState, action);
+    expect(state.value).toEqual(105);
+  });
+});

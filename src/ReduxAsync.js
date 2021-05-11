6d6ad2481979b67b86c25694a3bb6c6fa534f3d8
nabesuke <nabesuke5098@gmail.com>
2021-05-11 20:08:25 +0900

    [add]ReduxAsync.js &test-ReduxAsyncAPI.test.js

--- src/ReduxAsync.js
+++ src/ReduxAsync.js
@@ -2,16 +2,21 @@ import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import {
   selectCustomCount,
+  selectUsername,
   fetchDummy,
+  fetchJSON,
 } from "./features/customCounter/customCounterSlice";
 
 const ReduxAsync = () => {
   const count = useSelector(selectCustomCount);
+  const username = useSelector(selectUsername);
   const dispatch = useDispatch();
   return (
     <div>
       <span data-testid="count-value">{count}</span>
       <button onClick={() => dispatch(fetchDummy(5))}>FetchDummy</button>
+      {username && <h1>{username}</h1>}
+      <button onClick={() => dispatch(fetchJSON())}>FetchJSON</button>
     </div>
   );
 };
